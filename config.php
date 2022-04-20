95f920e98eddb17a6b3469da8a73f37dcb35e128
Alicio12 <bonnet.dylan11@gmail.com>
2022-04-20 02:04:12 +0200

    migration

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 
-$host = 'localhost';
+$host = 'bv2rebwf6zzsv341.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
 $dbname = 'hypnosevaluation';
-$username = 'root';
-$password = '';
-$dsn = "mysql:host=$host;dbname=hypnosevaluation";
+$username = 'g35nf51zxizrbii0';
+$password = 'jmghd4gna01jef6v';
+$dsn = "mysql://g35nf51zxizrbii0:jmghd4gna01jef6v@bv2rebwf6zzsv341.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/yi5e8ckpkufti8g2";
 
 try {
-    $pdo = new PDO($dsn, username:'root', password:'');
+    $pdo = new PDO($dsn, username:'g35nf51zxizrbii0', password:'jmghd4gna01jef6v');
 } catch (PDOException $e) {
     echo 'Impossible de se connecter à la base de données';
 }
