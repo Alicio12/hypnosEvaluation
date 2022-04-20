ed27add617f2968d832e7d786a5c1f46349d60d0
Alicio12 <bonnet.dylan11@gmail.com>
2022-04-20 02:13:29 +0200

    correctif mysql

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 
 $host = 'bv2rebwf6zzsv341.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-$dbname = 'hypnosevaluation';
+$dbname = 'yi5e8ckpkufti8g2';
 $username = 'g35nf51zxizrbii0';
 $password = 'jmghd4gna01jef6v';
-$dsn = "mysql://g35nf51zxizrbii0:jmghd4gna01jef6v@bv2rebwf6zzsv341.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/yi5e8ckpkufti8g2";
+$dsn = "mysql:host=$host;dbname=$dbname";
 
 try {
-    $pdo = new PDO($dsn, username:'g35nf51zxizrbii0', password:'jmghd4gna01jef6v');
+    $pdo = new PDO($dsn,$username,$password);
 } catch (PDOException $e) {
     echo 'Impossible de se connecter à la base de données';
 }
